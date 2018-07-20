c7f87076b3aa01b41a85b842d36a445976931791
Kaustubh Deshpande <kaustubhdeshpande@Kaustubhs-MacBook-Pro.local>
2018-07-20 09:52:35 -0500

    fist commit

--- stage-1/src/components/StoreFront/StoreFront.js
+++ stage-1/src/components/StoreFront/StoreFront.js
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './StoreFront.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./StoreFront.css";
 
 class StoreFront extends Component {
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            products: []
-        }
-    }
+    this.state = {
+      products: []
+    };
+  }
 
-    componentDidMount() {
-        axios.get("https://practiceapi.devmountain.com/products/")
-            .then((response) => {
-                this.setState({
-                    products: response
-                })
-            })
-    }
+  componentDidMount() {
+    axios.get("https://practiceapi.devmountain.com/products/").then(res => {
+      this.setState({
+        products: res.data
+      });
+    });
+  }
 
-    render() {
-        let productDisplay = this.state.products.map((element, index) => {
-            return (
-                <div className="product-container" key={index}>
-                    <h2>{element.title}</h2>
-                    <img src={element.image} alt="" />
-                    <h2>{element.desc}</h2>
-                    <h3>{"$" + element.price + ".00"}</h3>
-                    <button onClick={() => this.props.addToShoppingCart(element)}>Purchase!</button>
-                </div>
-            )
-        })
-        return (
-            <div className="storefront-container">
-                {productDisplay}
-            </div>
-        )
-    }
+  render() {
+    const { products } = this.state;
+    let productDisplay = products.map((e, i) => {
+      return (
+        <div className="product-container" key={i}>
+          <h2>{e.title}</h2>
+          <img src={e.image} alt="" />
+          <h2>{e.desc}</h2>
+          <h3>{"$" + e.price + ".00"}</h3>
+          <button onClick={() => this.props.addToShoppingCart(e)}>
+            Purchase!
+          </button>
+        </div>
+      );
+    });
+    return <div className="storefront-container">{productDisplay}</div>;
+  }
 }
 
 export default StoreFront;
--- stage-3/src/components/NavBar/NavBar.js
+++ stage-3/src/components/NavBar/NavBar.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react';
-import './NavBar.css';
-import Logo from '../../images/dev-mountain-logo.png'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./ShoppingCart.css";
+import { removeFromShoppingCart } from "../../redux/reducer";
+import { connect } from "react-redux";
 
-class NavBar extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="navbar-container">
-                    <div className="logo-container">
-                        <Link to="/"><img className="logo-image" src={Logo} alt="" /></Link>
-                    </div>
-
-                    <div className="navbar-links-container">
-                        <ul className="navbar-links">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/storefront">Store</Link></li>
-                            <li><Link to="/shoppingcart">Shopping Cart</Link></li>
-                        </ul>
-                    </div>
-                </div>
-                <hr />
+class ShoppingCart extends Component {
+  render() {
+    let shoppingCartDisplay = this.props.shoppingCart.map((element, index) => {
+      return (
+        <div className="shopping-cart-product-container" key={index}>
+          <img src={element.image} alt="" />
+          <div className="shopping-cart-info">
+            <h2>{element.title}</h2>
+            <h2>{"$" + element.price + ".00"}</h2>
+            <div className="shopping-cart-button-container">
+              <button
+                className="shopping-cart-button"
+                onClick={() => this.props.removeFromShoppingCart(index)}
+              >
+                Remove From Shopping Cart
+              </button>
             </div>
-        )
-    }
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className="shopping-cart-container">
+        {shoppingCartDisplay[0] ? (
+          shoppingCartDisplay
+        ) : (
+          <div className="go-buy-something">
+            <h1>Your shopping cart is empty! Go buy something!</h1>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default NavBar;
+function mapStateToProps(state) {
+  console.log(state);
+  return {
+    shoppingCart: state.shoppingCart
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { removeFromShoppingCart }
+)(ShoppingCart);
